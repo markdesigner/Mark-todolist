208daea6be3bfc111a59ea799eef9d92efef6e8e
markdesigner <weisheng1202@gmail.com>
2019-03-12 18:18:05 +0800

    第一次上傳

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Mark ToDoList</title>
+</head>
+<body>
+    <div class="wrap">
+        <div class="container">
+            <h1 id="big-title">打自己想打的標題</h1>
+            <ul>
+                <li>
+                    <input class="m-2" type="checkbox">
+                    <div class="item" contenteditable="true">what do you want to do?</div>
+                    <button class="delete ml-auto" type="button"><i class="fas fa-trash"></i></button>
+
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
